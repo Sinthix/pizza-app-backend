0f77a4b240e2075b6573c7e5eeb7e63ad687772a
Sinthix <ivo.ivan89@hotmail.com>
2024-12-07 13:59:20 +0000

    feat: adds crud functionalities to ingredient model

--- models/Ingredient.php
+++ models/Ingredient.php
@@ -13,50 +13,73 @@ class Ingredient {
         $this->connection = $db;
     }
 
-    // Get all ingredients
-    public function readAll() {
-        $query = "SELECT * FROM " . $this->table_name;
-        $stmt = $this->connection->prepare($query);
-        $stmt->execute();
-        return $stmt;
-    }
-
-    // Create ingredient
     public function create() {
-        $query = "INSERT INTO " . $this->table_name . "
-                  SET name=:name, cost_price=:cost_price, image_url=:image_url, randomization_percentage=:randomization_percentage";
+        $query = "
+            INSERT INTO " . $this->table . " (name, cost_price, image_url, randomisation_percentage)
+            VALUES (:name, :cost_price, :image_url, :randomisation_percentage);
+        ";
         $stmt = $this->connection->prepare($query);
 
-        $this->name = htmlspecialchars(strip_tags($this->name));
-        $this->cost_price = htmlspecialchars(strip_tags($this->cost_price));
-        $this->image_url = htmlspecialchars(strip_tags($this->image_url));
-        $this->randomization_percentage = htmlspecialchars(strip_tags($this->randomization_percentage));
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':cost_price', $this->cost_price);
+        $stmt->bindParam(':image_url', $this->image_url);
+        $stmt->bindParam(':randomisation_percentage', $this->randomisation_percentage);
 
-        $stmt->bindParam(":name", $this->name);
-        $stmt->bindParam(":cost_price", $this->cost_price);
-        $stmt->bindParam(":image_url", $this->image_url);
-        $stmt->bindParam(":randomization_percentage", $this->randomization_percentage);
-
-        if($stmt->execute()) {
-            return true;
-        }
-
-        return false;
+        return $stmt->execute();
     }
 
-    // Delete Ingredient
-    public function delete() {
-        $query = "DELETE FROM " . $this->table_name . "WHERE id = :id";
-        $stmt = $this->connection-prepare($query);
+    public function update() {
+        $query = "
+            UPDATE " . $this->table . "
+            SET name = :name, cost_price = :cost_price, image_url = :image_url, randomisation_percentage = :randomisation_percentage
+            WHERE id = :id;
+        ";
+        $stmt = $this->connection->prepare($query);
 
-        $this->id = htmlspecialchars(strip_tags($this->id));
+        $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':cost_price', $this->cost_price);
+        $stmt->bindParam(':image_url', $this->image_url);
+        $stmt->bindParam(':randomisation_percentage', $this->randomisation_percentage);
 
-        $stmt->bindParam(":id", $this->id);
+        return $stmt->execute();
+    }
 
-        if($stmt->execute()) {
-            return true;
-        }
+    public function delete($id) {
+        $query = "
+            DELETE FROM " . $this->table . "
+            WHERE id = :id;
+        ";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 
-        return false;
+        return $stmt->execute();
+    }
+
+    public function findAll() {
+        $query = "SELECT * FROM " . $this->table . ";";
+        $stmt = $this->connection->query($query);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function find($id) {
+        $query = "
+            SELECT * FROM " . $this->table . "
+            WHERE id = :id;
+        ";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function findRandomizable() {
+        $query = "
+            SELECT * FROM " . $this->table . "
+            WHERE randomisation_percentage > 0;
+        ";
+        $stmt = $this->connection->query($query);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
