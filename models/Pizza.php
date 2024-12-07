a956bc10afbc6cdf090a6754bed14d7c3e76bb2b
Sinthix <ivo.ivan89@hotmail.com>
2024-12-07 13:56:22 +0000

    feat: adds crud functionality to pizza model

--- models/Pizza.php
+++ models/Pizza.php
@@ -12,29 +12,122 @@ class Pizza {
         $this->connection = $db;
     }
 
-    public function readAll() {
-        $query = "SELECT * FROM " . $this->table_name;
-        $stmt = $this->connection->prepare($query);
-        $stmt->execute();
-        return $stmt;
-    }
-
     public function create() {
-        $query = "INSER INTO " . $this->table_name . " SET name=:name, selling_price=:selling_price, image_url=:image_url";
+        $query = "
+            INSERT INTO " . $this->table . " (name, selling_price, image_url)
+            VALUES (:name, :selling_price, :image_url);
+        ";
         $stmt = $this->connection->prepare($query);
 
-        $this->name = htmlspecialchars(strip_tags($this->name));
-        $this->selling_price = htmlspecialchars(strip_tags($this->selling_price));
-        $this->image_utl = htmlspecialchars(strip_tags($this->image_url));
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':selling_price', $this->selling_price);
+        $stmt->bindParam(':image_url', $this->image_url);
 
-        $stmt->bindParam(":name", $this->name);
-        $stmt->bindParam(":selling_price", $this->selling_price);
-        $stmt->bindParam(":image_url", $this->image_url);
+        if ($stmt->execute()) {
+            $this->id = $this->connection->lastInsertId();
 
-        if($stmt->execute()) {
+            if (!empty($this->ingredients)) {
+                $this->addIngredients($this->id, $this->ingredients);
+            }
             return true;
         }
 
         return false;
+    }
+
+    public function update() {
+        $query = "
+            UPDATE " . $this->table . "
+            SET name = :name, selling_price = :selling_price, image_url = :image_url
+            WHERE id = :id;
+        ";
+        $stmt = $this->connection->prepare($query);
+
+        $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':selling_price', $this->selling_price);
+        $stmt->bindParam(':image_url', $this->image_url);
+
+        if ($stmt->execute()) {
+            $this->removeIngredients($this->id);
+            if (!empty($this->ingredients)) {
+                $this->addIngredients($this->id, $this->ingredients);
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    public function delete($id) {
+        $this->removeIngredients($id);
+
+        $query = "
+            DELETE FROM " . $this->table . "
+            WHERE id = :id;
+        ";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+        return $stmt->execute();
+    }
+
+    public function findAll() {
+        $query = "
+            SELECT p.*, GROUP_CONCAT(pi.ingredient_id) AS ingredients
+            FROM " . $this->table . " p
+            LEFT JOIN pizza_ingredients pi ON p.id = pi.pizza_id
+            GROUP BY p.id;
+        ";
+        $stmt = $this->connection->query($query);
+
+        $pizzas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($pizzas as &$pizza) {
+            $pizza['ingredients'] = $pizza['ingredients'] ? explode(',', $pizza['ingredients']) : [];
+        }
+        return $pizzas;
+    }
+
+    public function find($id) {
+        $query = "
+            SELECT p.*, GROUP_CONCAT(pi.ingredient_id) AS ingredients
+            FROM " . $this->table . " p
+            LEFT JOIN pizza_ingredients pi ON p.id = pi.pizza_id
+            WHERE p.id = :id
+            GROUP BY p.id;
+        ";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $pizza = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($pizza) {
+            $pizza['ingredients'] = $pizza['ingredients'] ? explode(',', $pizza['ingredients']) : [];
+        }
+        return $pizza;
+    }
+
+    private function addIngredients($pizzaId, $ingredients) {
+        $query = "
+            INSERT INTO pizza_ingredients (pizza_id, ingredient_id)
+            VALUES (:pizza_id, :ingredient_id);
+        ";
+        $stmt = $this->connection->prepare($query);
+
+        foreach ($ingredients as $ingredientId) {
+            $stmt->bindParam(':pizza_id', $pizzaId, PDO::PARAM_INT);
+            $stmt->bindParam(':ingredient_id', $ingredientId, PDO::PARAM_INT);
+            $stmt->execute();
+        }
+    }
+
+    private function removeIngredients($pizzaId) {
+        $query = "
+            DELETE FROM pizza_ingredients
+            WHERE pizza_id = :pizza_id;
+        ";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':pizza_id', $pizzaId, PDO::PARAM_INT);
+        $stmt->execute();
     }
 }
