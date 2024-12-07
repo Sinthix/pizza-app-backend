d2d512db7888f9bb3041c4d4600231ac6d88ac8a
Sinthix <ivo.ivan89@hotmail.com>
2024-12-07 10:02:42 +0000

    feat: adds starting api routes

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once './config/database.php';
+require_once './controllers/PizzaController.php';
+//require_once './controllers/IngredientController.php';
+
+// Db connection
+$database = new Database();
+$db = $database->getConnection();
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$requestUri = $_SERVER["REQUEST_URI"];
+
+// Route requests based on URI
+if(strpos($requestUri, '/pizzas') !== false) {
+    $controller = new PizzaController($db, $requestMethod);
+} else {
+    header("HTTP/1.1 404 Not Found");
+    echo json_encode(["message" => "Endpoint not found."]);
+    exit();
+}
+
+// Process the request
+$controller->processRequest();
