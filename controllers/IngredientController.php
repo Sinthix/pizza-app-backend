9f739b44523ece3b10a5b4b5ff697c258af68b51
Sinthix <ivo.ivan89@hotmail.com>
2024-12-07 11:39:26 +0000

    feat: adds validation and proper response to ingredient controller

--- controllers/IngredientController.php
+++ controllers/IngredientController.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../models/Ingredient.php';
+include_once __DIR__ . '../models/Ingredient.php';
 
 class IngredientController {
     private $db;
@@ -13,7 +13,11 @@ class IngredientController {
     public function processRequest() {
         switch ($this->requestMethod) {
             case 'GET':
-                $this->getIngredients();
+                if(isset($_GET['id'])) {
+                    $response = $this-getIngredient($_GET['id']);
+                } else {
+                    $this->getAllIngredients();
+                }
                 break;
             case 'POST':
                 $this->createIngredient();
@@ -25,46 +29,89 @@ class IngredientController {
                 $this->notFoundResponse();
                 break;
         }
+
+        header($response['status_code_header']);
+        if($response['body']) {
+            echo $response['body'];
+        }
     }
 
-    public function getIngredients() {
+    public function getAllIngredients() {
         $ingredient = new Ingredient($this->db);
-        $result = $ingredient->readAll();
-        $ingredients = $result->fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($ingredients);
+        $result = $ingredient->findAll();
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function getIngredient($id) {
+        $ingredient = new Ingredient($this->db);
+        $result = $ingredient->find($id);
+
+        if(!$result) {
+            return $this->notFoundResponse();
+        }
+
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
     }
 
     private function createIngredient() {
         $input = json_decode(file_get_contents('php://input'), true);
-        $ingredient = new Ingredient($this->db);
+        if(!$this->validateIngredientInput($input)) {
+            return $this->unprocessableEntityResponse();
+        }
 
+        $ingredient = new Ingredient($this->db);
         $ingredient->name = $input['name'];
         $ingredient->cost_price = $input['cost_price'];
         $ingredient->image_url = $input['image_url'];
         $ingredient->randomization_percentage = $input['randomization_percentage'];
 
-        if($ingredient->create()) {
-            echo json_encode(['message' => 'Ingredient created']);
-        } else {
-            echo json_encode(['message' => 'Ingredient not created']);
-        }
+        $ingredient->create();
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = json_encode(['message' => 'Ingredient created']);
+        return $response;
     }
 
     private function deleteIngredient() {
         $input = json_decode(file_get_contents('php://input'), true);
-        $ingredient = new Ingredient($this->db);
-
-        $ingredient->id = $input['id'];
-
-        if($ingredient->delete()) {
-            echo json_encode(['message' => 'Ingredient deleted']);
-        } else {
-            echo json_encode(['message' => 'Ingredient not deleted']);
+        
+        if(!isset($input['id'])) {
+            return $this->unprocessableEntityResponse();
         }
+
+
+        $ingredient = new Ingredient($this->db);
+        $ingredient->delete($input['id']);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode(['message' => 'Ingredient deleted']);
+        return $response;
+    }
+
+    private function validateIngredientInput($input) {
+        if(
+            !isset($input['name']) ||
+            !isset($input['cost_price']) ||
+            !isset($input['randomization_percentage']) ||
+            !is_numeric($input['cost_price']) ||
+            !is_numeric($input['randomization_percentage'])
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    private function unprocessableEntityResponse() {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode(['message' => 'Invalid input']);
+        return $response;
     }
 
     private function notFoundResponse() {
-        header("HTTP/1.1 404 Not Found");
-        echo json_encode(['message' => 'Endpoint not found']);
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = json_encode(['message' => 'Not iFound']);
+        return $response;
     }
 }
